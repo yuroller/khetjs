e057f9ec2da2a09cee83bb10b149179d370576b3
Yuri Valentini <yuroller@gmail.com>
2013-02-25 18:16:13 +0100

    pieces refactoring and tests

--- test/khet_spec.js
+++ test/khet_spec.js
@@ -7,32 +7,56 @@
  
 'use strict';
 
-var khet = require("./lib/khet");
+var khet = require("../lib/khet");
 var assert = require('assert');
 
-suite('getColorFromSurface', function () {
+///////////////////////////////////////////////////////////////////////////
+// Surface
+///////////////////////////////////////////////////////////////////////////
+
+suite('Surface', function () {
   var surface;
   setup(function () {
-    surface = ['RBS', 'SBR'];
+    surface = new khet.Surface(['R.S', 'S.R']);
   });
   test('should return red color at (0,0)', function () {
-    assert.equal(khet.getColorFromSurface(surface, 0, 0), khet.Color.RED);
+    assert.equal(surface.getTileColor(0, 0), khet.Color.RED);
   });
   test('should return silver color at (0,1)', function () {
-    assert.equal(khet.getColorFromSurface(surface, 0, 1), khet.Color.SILVER);
+    assert.equal(surface.getTileColor(0, 1), khet.Color.SILVER);
+  });    
+  test('should return none color at (1,0)', function () {
+    assert.equal(surface.getTileColor(1, 0), khet.Color.NONE);
   });    
 });
 
-suite('getPiecesFromSetup', function () {
-  var s;
-  setup(function(){
-    s = [{x: 4, y: 0, t: khet.PieceType.OBELISK, c: khet.Color.SILVER, o: khet.Orientation.NONE}];
+
+///////////////////////////////////////////////////////////////////////////
+// createPieceFromSetup
+///////////////////////////////////////////////////////////////////////////
+
+suite('createPieceFromSetup', function () {
+  var piecesSetup = [
+    {x: 7, y: 0, t: khet.PieceType.PIRAMID,      c: khet.Color.SILVER,    o: khet.Orientation.SE},
+    {x: 4, y: 7, t: khet.PieceType.PHARAOH,      c: khet.Color.RED,       o: khet.Orientation.NONE}
+  ];
+  test('should return piece at (7,0)', function () {
+    var p = khet.createPieceFromSetup(piecesSetup, 7, 0);
+    assert.equal(p.getPieceType(), khet.PieceType.PIRAMID);
+    assert.equal(p.getColor(), khet.Color.SILVER);
+    assert.equal(p.getOrientation(), khet.Orientation.SE);
   });
-  test('should return [OBELISK] at (4,0)', function(){
-    var p = khet.getPiecesFromSetup(s, 4, 0);
-    assert.equal(p.length, 1);
-    assert.equal(p[0].t, khet.PieceType.OBELISK);
-    assert.equal(p[0].c, khet.Color.SILVER);
-    assert.equal(p[0].o, khet.Orientation.NONE);
+  test('should return piece at (4,7)', function () {
+    var p = khet.createPieceFromSetup(piecesSetup, 4, 7);
+    assert.equal(p.getPieceType(), khet.PieceType.PHARAOH);
+    assert.equal(p.getColor(), khet.Color.RED);
+    assert.equal(p.getOrientation(), khet.Orientation.NONE);
+  });
+  test('should return no piece at (3,4)', function () {
+    var p = khet.createPieceFromSetup(piecesSetup, 3, 4);
+    assert.equal(p.getPieceType(), khet.PieceType.NONE);
+    assert.equal(p.getColor(), khet.Color.NONE);
+    assert.equal(p.getOrientation(), khet.Orientation.NONE);
   });
 });
+
